b11e9ee491dae3a52721f1f3488c38d00c921d4d
Ray Toal <rtoal@lmu.edu>
2025-07-03 23:03:33 -0700

    Address some style warnings

--- ada/edp/src/buffers.ads
+++ ada/edp/src/buffers.ads
@@ -1,39 +1,39 @@
 ------------------------------------------------------------------------------
--- buffers.ads
+--  buffers.ads
 --
--- A generic package  for bounded, blocking FIFO queues (buffers).  It exports
--- a proected type called 'Buffer'.  Note that the  maximum size of any buffer 
--- of this type is taken from a single generic package parameter.
+--  A generic package for bounded, blocking FIFO queues (buffers). It exports
+--  a proected type called 'Buffer'. Note that the maximum size of any buffer
+--  of this type is taken from a single generic package parameter.
 --
--- Generic Parameters:
+--  Generic Parameters:
 --
---   Item        the desired type of the buffer elements.
---   Size        the maximum size of a buffer of type Buffer.
+--    Item        the desired type of the buffer elements.
+--    Size        the maximum size of a buffer of type Buffer.
 --
--- Entries:   
+--  Entries:
 --
---   Write (I)   write item I to the buffer.
---   Read (I)    read into item I from the buffer.
+--    Write (I)   write item I to the buffer.
+--    Read (I)    read into item I from the buffer.
 ------------------------------------------------------------------------------
 
 generic
 
-  type Item is private;
-  Size: Positive;
+   type Item is private;
+   Size : Positive;
 
 package Buffers is
 
-  subtype Index is Integer range 0..Size-1;
-  type Item_Array is array (Index) of Item;
+   subtype Index is Integer range 0 .. Size - 1;
+   type Item_Array is array (Index) of Item;
 
-  protected type Buffer is
-    entry Write (I: Item);
-    entry Read (I: out Item);
-  private
-    Data  : Item_Array;
-    Front : Index := 0;                     -- index of head (when not empty)
-    Back  : Index := 0;                     -- index of next free slot
-    Count : Integer range 0..Size := 0;     -- number of items currently in
-  end Buffer;
+   protected type Buffer is
+      entry Write (I : Item);
+      entry Read (I : out Item);
+   private
+      Data  : Item_Array;
+      Front : Index := 0;                   -- index of head (when not empty)
+      Back  : Index := 0;                   -- index of next free slot
+      Count : Integer range 0 .. Size := 0; -- number of items currently in
+   end Buffer;
 
 end Buffers;
--- ada/edp/src/orders.ads
+++ ada/edp/src/orders.ads
@@ -1,18 +1,18 @@
 ------------------------------------------------------------------------------
--- orders.ads
+--  orders.ads
 --
--- This package contains the public data type Order and a heat lamp to store
--- finished orders under.
+--  This package contains the public data type Order and a heat lamp to store
+--  finished orders under.
 --
--- Types:
+--  Types:
 --
---   Order       An order is simply a pair consisting of a meal and the
+--    Order      An order is simply a pair consisting of a meal and the
 --               philosopher who ordered it.
 --
--- Objects:
+--  Objects:
 --
---   Heat_Lamp   When cooks are finished preparing an order they place it
---               under this heat lamp.  A waiter should come by and pick it up
+--    Heat_Lamp  When cooks are finished preparing an order they place it
+--               under this heat lamp. A waiter should come by and pick it up
 --               and take it to the table. This object is represented as a
 --               bounded blocking queue with a capacity of five orders.
 ------------------------------------------------------------------------------
@@ -22,19 +22,19 @@ use Names, Meals;
 
 package Orders is
 
-  type Order is record
-    Diner : Philosopher_Name;               -- Who ordered it
-    Food  : Meal;                           -- What they ordered
-  end record;
+   type Order is record
+      Diner : Philosopher_Name;               -- Who ordered it
+      Food  : Meal;                           -- What they ordered
+   end record;
 
-  -- The order bin and the heat lamp are simply queues of orders, so first
-  -- instantiate the generic package for blocking queues:
+   --  The order bin and the heat lamp are simply queues of orders, so first
+   --  instantiate the generic package for blocking queues:
 
-  package Order_Buffers is new Buffers (Order, 5);
-  use Order_Buffers;
+   package Order_Buffers is new Buffers (Order, 5);
+   use Order_Buffers;
 
-  -- Now declare the buffer objects:
+   --  Now declare the buffer objects:
 
-  Heat_Lamp : Buffer;
+   Heat_Lamp : Buffer;
 
 end Orders;
